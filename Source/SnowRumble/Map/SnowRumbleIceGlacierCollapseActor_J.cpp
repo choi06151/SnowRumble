e2dee52b79ccd4bbb6e54cc828e2a6fe4a368dd1
pgueue110-ctrl <pgueue110@gmail.com>
2026-08-21 17:04:06 +0900

    얼음빙하맵 추락기능 + task 수정

--- Source/SnowRumble/Map/SnowRumbleIceGlacierCollapseActor_J.cpp
+++ Source/SnowRumble/Map/SnowRumbleIceGlacierCollapseActor_J.cpp
@@ -79,17 +79,17 @@ void ASnowRumbleIceGlacierCollapseActor::InitializeCollapseTargets()
 	Group1TargetStates.Reset();
 	Group2TargetStates.Reset();
 
-	InitializeCollapseGroup(CollapseGroup1, Group1TargetStates);
-	InitializeCollapseGroup(CollapseGroup2, Group2TargetStates);
+	InitializeCollapseGroup(Group1Pieces, Group1TargetStates);
+	InitializeCollapseGroup(Group2Pieces, Group2TargetStates);
 }
 
 void ASnowRumbleIceGlacierCollapseActor::InitializeCollapseGroup(
-	const TArray<TObjectPtr<AActor>>& SourceActors,
+	const TArray<FIceGlacierCollapsePiece>& SourcePieces,
 	TArray<FIceGlacierCollapseTargetState>& OutTargetStates)
 {
-	for (const TObjectPtr<AActor>& TargetActorReference : SourceActors)
+	for (const FIceGlacierCollapsePiece& Piece : SourcePieces)
 	{
-		AActor* TargetActor = TargetActorReference.Get();
+		AActor* TargetActor = Piece.TargetActor.Get();
 		if (!IsValid(TargetActor))
 		{
 			continue;
@@ -97,6 +97,13 @@ void ASnowRumbleIceGlacierCollapseActor::InitializeCollapseGroup(
 
 		FIceGlacierCollapseTargetState TargetState;
 		TargetState.Actor = TargetActor;
+		TargetState.PieceSettings.StartDelaySeconds = Piece.StartDelaySeconds;
+		TargetState.PieceSettings.WarningDurationSeconds =
+			Piece.WarningDurationSeconds;
+		TargetState.PieceSettings.ShakeAmplitude = Piece.ShakeAmplitude;
+		TargetState.PieceSettings.ShakeFrequency = Piece.ShakeFrequency;
+		TargetState.PieceSettings.FallDurationSeconds = Piece.FallDurationSeconds;
+		TargetState.PieceSettings.SinkDistance = Piece.SinkDistance;
 		TargetState.InitialTransform = TargetActor->GetActorTransform();
 
 		TArray<UPrimitiveComponent*> PrimitiveComponents;
@@ -209,34 +216,210 @@ float ASnowRumbleIceGlacierCollapseActor::CalculateGroup2Alpha(
 void ASnowRumbleIceGlacierCollapseActor::ApplyCollapseState(
 	float RoundElapsedSeconds)
 {
+	float Group1TimeSeconds = Group1StartSeconds - 1.0f;
+	float Group2TimeSeconds = Group2StartSeconds - 1.0f;
+
+	switch (CurrentCollapsePhase)
+	{
+	case ESnowRumbleIceGlacierCollapsePhase::Group1Sinking:
+		Group1TimeSeconds = RoundElapsedSeconds;
+		break;
+	case ESnowRumbleIceGlacierCollapsePhase::Group1Complete:
+		Group1TimeSeconds = Group1EndSeconds;
+		break;
+	case ESnowRumbleIceGlacierCollapsePhase::Group2Sinking:
+		Group1TimeSeconds = Group1EndSeconds;
+		Group2TimeSeconds = RoundElapsedSeconds;
+		break;
+	case ESnowRumbleIceGlacierCollapsePhase::Complete:
+		Group1TimeSeconds = Group1EndSeconds;
+		Group2TimeSeconds = Group2EndSeconds;
+		break;
+	case ESnowRumbleIceGlacierCollapsePhase::Stable:
+	default:
+		break;
+	}
+
 	ApplyCollapseGroup(
 		Group1TargetStates,
-		CalculateGroup1Alpha(RoundElapsedSeconds));
+		Group1TimeSeconds,
+		Group1StartSeconds,
+		Group1EndSeconds);
 	ApplyCollapseGroup(
 		Group2TargetStates,
-		CalculateGroup2Alpha(RoundElapsedSeconds));
+		Group2TimeSeconds,
+		Group2StartSeconds,
+		Group2EndSeconds);
 }
 
 void ASnowRumbleIceGlacierCollapseActor::ApplyCollapseGroup(
 	TArray<FIceGlacierCollapseTargetState>& TargetStates,
-	float Alpha)
+	float RoundElapsedSeconds,
+	float GroupStartSeconds,
+	float GroupEndSeconds)
 {
 	for (FIceGlacierCollapseTargetState& TargetState : TargetStates)
 	{
-		ApplyTargetTransform(TargetState, Alpha);
+		float WarningAlpha = 0.0f;
+		float FallAlpha = 0.0f;
+		const EIceGlacierCollapsePieceState PieceState = CalculatePieceState(
+			TargetState.PieceSettings,
+			RoundElapsedSeconds,
+			GroupStartSeconds,
+			GroupEndSeconds,
+			WarningAlpha,
+			FallAlpha);
 
-		if (Alpha >= 1.0f && bDisableCollisionAfterFullySunk)
+		switch (PieceState)
 		{
-			DisableTargetCollision(TargetState);
-		}
-		else
+		case EIceGlacierCollapsePieceState::Warning:
 		{
+			const float WarningStartSeconds = GroupStartSeconds +
+				FMath::Max(0.0f, TargetState.PieceSettings.StartDelaySeconds);
+			const float WarningElapsedSeconds =
+				FMath::Max(0.0f, RoundElapsedSeconds - WarningStartSeconds);
+			ApplyWarningTransform(
+				TargetState,
+				WarningElapsedSeconds,
+				WarningAlpha);
 			RestoreTargetCollision(TargetState);
+			break;
+		}
+		case EIceGlacierCollapsePieceState::Falling:
+			ApplyFallingTransform(TargetState, FallAlpha);
+			RestoreTargetCollision(TargetState);
+			break;
+		case EIceGlacierCollapsePieceState::Done:
+			ApplyFallingTransform(TargetState, 1.0f);
+			if (bDisableCollisionAfterFullySunk)
+			{
+				DisableTargetCollision(TargetState);
+			}
+			else
+			{
+				RestoreTargetCollision(TargetState);
+			}
+			break;
+		case EIceGlacierCollapsePieceState::Idle:
+		default:
+			ApplyInitialTransform(TargetState);
+			RestoreTargetCollision(TargetState);
+			break;
 		}
 	}
 }
 
-void ASnowRumbleIceGlacierCollapseActor::ApplyTargetTransform(
+EIceGlacierCollapsePieceState
+ASnowRumbleIceGlacierCollapseActor::CalculatePieceState(
+	const FIceGlacierCollapsePieceRuntimeSettings& PieceSettings,
+	float RoundElapsedSeconds,
+	float GroupStartSeconds,
+	float GroupEndSeconds,
+	float& OutWarningAlpha,
+	float& OutFallAlpha) const
+{
+	OutWarningAlpha = 0.0f;
+	OutFallAlpha = 0.0f;
+
+	if (RoundElapsedSeconds >= GroupEndSeconds)
+	{
+		OutFallAlpha = 1.0f;
+		return EIceGlacierCollapsePieceState::Done;
+	}
+
+	const float WarningStartSeconds = GroupStartSeconds +
+		FMath::Max(0.0f, PieceSettings.StartDelaySeconds);
+	if (RoundElapsedSeconds < WarningStartSeconds)
+	{
+		return EIceGlacierCollapsePieceState::Idle;
+	}
+
+	const float WarningDurationSeconds =
+		FMath::Max(0.0f, PieceSettings.WarningDurationSeconds);
+	const float WarningEndSeconds =
+		WarningStartSeconds + WarningDurationSeconds;
+	if (WarningDurationSeconds > KINDA_SMALL_NUMBER &&
+		RoundElapsedSeconds < WarningEndSeconds)
+	{
+		OutWarningAlpha = FMath::Clamp(
+			(RoundElapsedSeconds - WarningStartSeconds) /
+				WarningDurationSeconds,
+			0.0f,
+			1.0f);
+		return EIceGlacierCollapsePieceState::Warning;
+	}
+
+	OutWarningAlpha = 1.0f;
+
+	const float FallDurationSeconds =
+		FMath::Max(0.0f, PieceSettings.FallDurationSeconds);
+	if (FallDurationSeconds <= KINDA_SMALL_NUMBER)
+	{
+		OutFallAlpha = 1.0f;
+		return EIceGlacierCollapsePieceState::Done;
+	}
+
+	const float FallEndSeconds = WarningEndSeconds + FallDurationSeconds;
+	if (RoundElapsedSeconds < FallEndSeconds)
+	{
+		OutFallAlpha = FMath::Clamp(
+			(RoundElapsedSeconds - WarningEndSeconds) / FallDurationSeconds,
+			0.0f,
+			1.0f);
+		return EIceGlacierCollapsePieceState::Falling;
+	}
+
+	OutFallAlpha = 1.0f;
+	return EIceGlacierCollapsePieceState::Done;
+}
+
+void ASnowRumbleIceGlacierCollapseActor::ApplyWarningTransform(
+	FIceGlacierCollapseTargetState& TargetState,
+	float WarningElapsedSeconds,
+	float WarningAlpha)
+{
+	AActor* TargetActor = TargetState.Actor.Get();
+	if (!IsValid(TargetActor))
+	{
+		return;
+	}
+
+	const float ShakeAmplitude = TargetState.PieceSettings.ShakeAmplitude;
+	const float ShakeFrequency = TargetState.PieceSettings.ShakeFrequency;
+	if (ShakeAmplitude <= KINDA_SMALL_NUMBER ||
+		ShakeFrequency <= KINDA_SMALL_NUMBER)
+	{
+		ApplyInitialTransform(TargetState);
+		return;
+	}
+
+	const float ClampedWarningAlpha = FMath::Clamp(WarningAlpha, 0.0f, 1.0f);
+	const float ShakeStrength = ClampedWarningAlpha * ClampedWarningAlpha;
+	const float ShakePhase = WarningElapsedSeconds * ShakeFrequency * 2.0f * PI;
+
+	FTransform NewTransform = TargetState.InitialTransform;
+	FVector NewLocation = TargetState.InitialTransform.GetLocation();
+	NewLocation.X += FMath::Sin(ShakePhase) * ShakeAmplitude * 0.65f * ShakeStrength;
+	NewLocation.Y += FMath::Cos(ShakePhase * 1.31f) * ShakeAmplitude * 0.45f * ShakeStrength;
+	NewTransform.SetLocation(NewLocation);
+
+	const float RotationAmplitudeDegrees =
+		FMath::Min(1.5f, ShakeAmplitude * 0.08f) * ShakeStrength;
+	FRotator NewRotation = TargetState.InitialTransform.GetRotation().Rotator();
+	NewRotation.Roll +=
+		FMath::Sin(ShakePhase * 0.87f) * RotationAmplitudeDegrees;
+	NewRotation.Pitch +=
+		FMath::Cos(ShakePhase * 1.11f) * RotationAmplitudeDegrees;
+	NewTransform.SetRotation(NewRotation.Quaternion());
+
+	TargetActor->SetActorTransform(
+		NewTransform,
+		false,
+		nullptr,
+		ETeleportType::TeleportPhysics);
+}
+
+void ASnowRumbleIceGlacierCollapseActor::ApplyFallingTransform(
 	FIceGlacierCollapseTargetState& TargetState,
 	float Alpha)
 {
@@ -248,10 +431,27 @@ void ASnowRumbleIceGlacierCollapseActor::ApplyTargetTransform(
 
 	FTransform NewTransform = TargetState.InitialTransform;
 	FVector NewLocation = TargetState.InitialTransform.GetLocation();
-	NewLocation.Z -= SinkDistance * FMath::Clamp(Alpha, 0.0f, 1.0f);
+	NewLocation.Z -= TargetState.PieceSettings.SinkDistance *
+		FMath::Clamp(Alpha, 0.0f, 1.0f);
 	NewTransform.SetLocation(NewLocation);
 	TargetActor->SetActorTransform(
 		NewTransform,
+		false,
+		nullptr,
+		ETeleportType::TeleportPhysics);
+}
+
+void ASnowRumbleIceGlacierCollapseActor::ApplyInitialTransform(
+	FIceGlacierCollapseTargetState& TargetState)
+{
+	AActor* TargetActor = TargetState.Actor.Get();
+	if (!IsValid(TargetActor))
+	{
+		return;
+	}
+
+	TargetActor->SetActorTransform(
+		TargetState.InitialTransform,
 		false,
 		nullptr,
 		ETeleportType::TeleportPhysics);
